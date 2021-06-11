60e55b4b49c7091702dabb9108329b7a8e44f1da
Vezil <44679435+Vezil@users.noreply.github.com>
2021-06-11 19:58:14 +0200

    Feature/middleware and controllers (#1)
    * add controller and model
    
    * add crud

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,22 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import todoRoutes from './routes/todos';
+import { json } from 'body-parser';
 
 const app = express();
+
+app.use(json());
+
+app.use('/todos', todoRoutes);
+
+app.use(
+    (
+        error: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction
+    ) => {
+        response.status(500).json({ message: error.message });
+    }
+);
 
 app.listen(5000);
---
+++ src/routes/todos.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import {
+    createTodo,
+    getTodos,
+    updateTodo,
+    removeTodo
+} from '../controllers/todos';
+
+const router = Router();
+
+router.get('/', getTodos);
+
+router.post('/', createTodo);
+
+router.patch('/:id', updateTodo);
+
+router.delete('/:id', removeTodo);
+
+export default router;
